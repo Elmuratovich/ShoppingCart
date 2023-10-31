53aa401365cf0de66841626d28e5e5531d5b6794
Makhmud <makhmudkuzikaev@gmail.com>
2023-10-31 17:18:23 +0600

    create admin service

--- Shop.Admin/Shop.Admin/Services/AdminPanelService.cs
+++ Shop.Admin/Shop.Admin/Services/AdminPanelService.cs
@@ -1,6 +1,22 @@
-﻿namespace Shop.Admin.Services
+﻿using Microsoft.AspNetCore.Components;
+using Shop.DataModels.CustomModels;
+using System.Net.Http;
+
+namespace Shop.Admin.Services
 {
-    public class AdminPanelService:IAdminPanelService
+    public class AdminPanelService : IAdminPanelService
     {
+        private readonly HttpClient httpClient;
+
+        public AdminPanelService(HttpClient _httpClient)
+        {
+            httpClient = _httpClient;
+        }
+
+        public async Task<ResponseModel> AdminLogin(LoginModel loginModel)
+        {
+            // Make sure to specify the expected response type here (ResponseModel).
+            return await httpClient.PostJsonAsync<ResponseModel>("api/admin/AdminLogin", loginModel);
+        }
     }
 }
